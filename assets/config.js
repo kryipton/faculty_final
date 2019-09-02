ecac4ee264ac9fe6a7b0a5661a1cc9b77679b985
JavidLalayev <cavidleleyev9a12014@gmail.com>
2019-09-02 14:18:29 +0400

    sayt bitti

--- assets/config.js
+++ assets/config.js
@@ -3,6 +3,8 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
+var base_url = window.location.origin;
+
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
@@ -10,11 +12,11 @@ CKEDITOR.editorConfig = function( config ) {
 
 
     // ...
-    config.filebrowserBrowseUrl = 'http://localhost/faculty/assets/kcfinder/browse.php?opener=ckeditor&type=files';
-    config.filebrowserImageBrowseUrl = 'http://localhost/faculty/assets/kcfinder/browse.php?opener=ckeditor&type=images';
-    config.filebrowserFlashBrowseUrl = 'http://localhost/faculty/assets/kcfinder/browse.php?opener=ckeditor&type=flash';
-    config.filebrowserUploadUrl = 'http://localhost/faculty/assets/kcfinder/upload.php?opener=ckeditor&type=files';
-    config.filebrowserImageUploadUrl = 'http://localhost/faculty/assets/kcfinder/upload.php?opener=ckeditor&type=images';
-    config.filebrowserFlashUploadUrl = 'http://localhost/faculty/assets/kcfinder/http://localhost/faculty/upload.php?opener=ckeditor&type=flash';
+    config.filebrowserBrowseUrl =  base_url + '/assets/kcfinder/browse.php?opener=ckeditor&type=files';
+    config.filebrowserImageBrowseUrl = base_url +  '/assets/kcfinder/browse.php?opener=ckeditor&type=images';
+    config.filebrowserFlashBrowseUrl = base_url +  '/assets/kcfinder/browse.php?opener=ckeditor&type=flash';
+    config.filebrowserUploadUrl = base_url +  '/assets/kcfinder/upload.php?opener=ckeditor&type=files';
+    config.filebrowserImageUploadUrl = base_url +  '/assets/kcfinder/upload.php?opener=ckeditor&type=images';
+    config.filebrowserFlashUploadUrl = base_url +  '/assets/kcfinder/http://localhost/faculty/upload.php?opener=ckeditor&type=flash';
 // ...
 };
